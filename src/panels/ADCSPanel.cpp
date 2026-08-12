6bcf0e5cb1c4ea1ac29b01766aa62d69ef180a13
vcampos4545 <vcampos4545@gmail.com>
2026-08-12 17:56:40 -0400

    visual: mission time in gui window

--- src/panels/ADCSPanel.cpp
+++ src/panels/ADCSPanel.cpp
@@ -7,6 +7,7 @@
 #include "OrbitPanel.h"
 #include "GroundStationsPanel.h"
 #include "VisualizationPanel.h"
+#include <rigidbody/orbit/OrbitTime.h>
 #include <imgui.h>
 
 void drawADCSPanel(FlightSoftware &flightSoftware, Satellite &sat,
@@ -22,6 +23,32 @@ void drawADCSPanel(FlightSoftware &flightSoftware, Satellite &sat,
   ImGui::SetNextWindowPos(ImVec2(20, 20), ImGuiCond_FirstUseEver);
   ImGui::SetNextWindowSize(ImVec2(380, 560), ImGuiCond_FirstUseEver);
   ImGui::Begin("Satellite ADCS");
+
+  // Persistent header, above the tab bar so it's visible regardless of
+  // which tab is selected -- current mission clock (UTC, derived from
+  // currentJd) and elapsed mission time (orbitState.missionTimeS, seconds
+  // since EpochControls' commanded epoch -- see OrbitTime.h), the same two
+  // clocks the rest of this panel's orbital/ground-station math runs on.
+  {
+    int year, month, day, hour, minute;
+    double second;
+    OrbitTime::calendarDate(currentJd, year, month, day, hour, minute, second);
+
+    double missionTimeS = orbitState.missionTimeS;
+    int missionDays = static_cast<int>(missionTimeS / 86400.0);
+    double remS = missionTimeS - missionDays * 86400.0;
+    int missionHours = static_cast<int>(remS / 3600.0);
+    remS -= missionHours * 3600.0;
+    int missionMinutes = static_cast<int>(remS / 60.0);
+    remS -= missionMinutes * 60.0;
+
+    ImGui::Text("%04d-%02d-%02d %02d:%02d:%02.0f UTC", year, month, day, hour, minute, second);
+    ImGui::SameLine();
+    ImGui::TextDisabled("|");
+    ImGui::SameLine();
+    ImGui::Text("Mission Time: %dd %02dh %02dm %02.0fs", missionDays, missionHours, missionMinutes, remS);
+    ImGui::Separator();
+  }
 
   if (ImGui::BeginTabBar("ADCSTabs"))
   {
